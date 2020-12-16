1a0e9826c8b924cb9e494df0e21c31da3362ceac
bgwrm <13susotub.mtkz@gmail.com>
2020-12-16 21:49:09 +0900

    Update memo.py

--- memo.py
+++ memo.py
@@ -3,3 +3,12 @@ n = int(input())
 a, b = (int(_) for _ in input().split())
 s = [int(_) for _ in input().split()]
 xy = [[int(_) for _ in input().split()] for i in range(n)]
+mod = 10 ** 9 + 7
+
+# 最大公約数
+import math
+math.gcd(x, y)
+
+# 最小公倍数
+def lcm(x, y):
+    return (x * y) // math.gcd(x, y)
