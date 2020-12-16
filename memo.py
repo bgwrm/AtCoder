f707dab00eedf1d58813104cc65f3fb38b900933
bgwrm <13susotub.mtkz@gmail.com>
2020-12-16 22:20:12 +0900

    Update memo.py

--- memo.py
+++ memo.py
@@ -7,8 +7,15 @@ mod = 10 ** 9 + 7
 
 # 最大公約数
 import math
+x = 0
+y = 0
 math.gcd(x, y)
 
 # 最小公倍数
 def lcm(x, y):
     return (x * y) // math.gcd(x, y)
+
+# 組み合わせリスト
+import itertools
+l = []
+c = list(itertools.combinations(l,2))
