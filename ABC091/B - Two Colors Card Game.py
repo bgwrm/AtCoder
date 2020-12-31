2d8e84e81e7a8748f5a35a5473e0a5fbaa57476f
bgwrm <13susotub.mtkz@gmail.com>
2020-12-31 20:33:24 +0900

    Create B - Two Colors Card Game.py

---
+++ ABC091/B - Two Colors Card Game.py
@@ -0,0 +1,8 @@
+n = int(input())
+s = [input() for _ in range(n)]
+m = int(input())
+t = [input() for _ in range(m)]
+ans = 0
+for i in set(s):
+    ans = max(ans, s.count(i)-t.count(i))
+print(ans)
