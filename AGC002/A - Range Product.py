aeb937ff98dad061b5c34ff591f6950869f37ae7
bgwrm <13susotub.mtkz@gmail.com>
2021-01-05 00:22:52 +0900

    Create A - Range Product.py

---
+++ AGC002/A - Range Product.py
@@ -0,0 +1,9 @@
+a, b = (int(_) for _ in input().split())
+if a > 0 and b > 0:
+    print('Positive')
+elif a <= 0 and b >= 0:
+    print('Zero')
+elif (b-a)%2 == 0:
+    print('Negative')
+else:
+    print('Positive')
