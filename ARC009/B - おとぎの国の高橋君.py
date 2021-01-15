65bae84d543bd8b64c9ad1bff246935d13fc027d
bgwrm <13susotub.mtkz@gmail.com>
2021-01-15 23:46:38 +0900

    Create B - おとぎの国の高橋君.py

---
+++ ARC009/B - おとぎの国の高橋君.py
@@ -0,0 +1,6 @@
+b = [int(_) for _ in input().split()]
+n = int(input())
+a = [input() for _ in range(n)]
+d = str.maketrans({str(b[i]):str(i) for i in range(10)})
+a.sort(key=lambda  x:int(x.translate(d)))
+print(*a, sep='\n')
