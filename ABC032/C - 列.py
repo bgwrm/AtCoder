599a6af97ec217e2171a3002ed54a704935feed1
bgwrm <13susotub.mtkz@gmail.com>
2021-01-23 04:42:52 +0900

    Create C - 列.py

---
+++ ABC032/C - 列.py
@@ -0,0 +1,14 @@
+n, k = (int(_) for _ in input().split())
+s = [int(input()) for _ in range(n)]
+ans, c, j, p = 0, 0, 0, 1
+for i in range(n):
+    p *= s[i]
+    if p == 0:
+        ans = n
+        break
+    elif p <= k:
+        ans = max(i-j+1, ans)
+    else:
+        p //= s[j]
+        j += 1
+print(ans)
