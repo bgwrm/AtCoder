8f29ffea7061dabc8beb8fbaa3d6c5d3a8a0e6cf
bgwrm <13susotub.mtkz@gmail.com>
2021-02-03 22:32:05 +0900

    Update integer.py

--- integer.py
+++ integer.py
@@ -46,3 +46,20 @@ def is_prime(num):
         if num % i == 0:
             return False
     return True
+
+# 素因数分解
+def prime_factorize(n):
+    a = []
+    while n % 2 == 0:
+        a.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            a.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        a.append(n)
+    return a
