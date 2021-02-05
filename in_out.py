db3ea86e5e339019236671a6d1eec17ca945592c
bgwrm <13susotub.mtkz@gmail.com>
2021-02-05 21:29:34 +0900

    Update in_out.py

--- in_out.py
+++ in_out.py
@@ -30,6 +30,19 @@ print([chr(ord("a")+i) for i in range(26)])
 # リスト読み込みと同時に添え字(_i)もつける
 l = [(int(_), i) for i, _ in enumerate(input().split(), 1)]
 
+# キュー
+# 先頭(終端)の要素に対する挿入・削除
+from collections import deque
+d = deque([l])
+d.appendleft('element') # 先頭に要素を挿入
+d.append('element') # 終端に要素を挿入
+d.extendleft(['k', 'j']) # 先頭にリストを挿入
+d.extend(['p', 'q']) # 終端にリストを挿入
+d.popleft() # 先頭から要素を取り出す
+d.pop() # 終端から要素を取り出す
+d.rotate() # 右に要素をローテートさせる
+d.rotate(-1) # 引数でいくつ動かすか指定できる(-1は左に1つ)
+
 # 優先度付きキュー
 # 計算量O(logN)で最小値を取り出せる
 # 最大値で使いたい場合は予め-1を掛けておく
@@ -42,3 +55,13 @@ heapq.heappush(l, n) # 要素を挿入
 li = [1,2,1,3,1]
 indexes = [i for i, x in enumerate(list) if x == 1]
 print(indexes) # [0, 2, 4]
+
+# 正規表現
+import re
+p = re.compile('[a-z]+')
+s = 'abc'
+if p.fullmatch(s):
+    print('match')
+else:
+    print('no match')
+# match
