c8cda5cb3ef70bc1483c349c6f1675edfd5c1c59
bgwrm <13susotub.mtkz@gmail.com>
2021-02-18 22:58:34 +0900

    Update C - Lining Up.py

--- ABC050/C - Lining Up.py
+++ ABC050/C - Lining Up.py
@@ -1,13 +1,13 @@
 import heapq
 n = int(input())
 a = [int(_) for _ in input().split()]
-mod = 10**9+7
 heapq.heapify(a)
-if n%2 and heapq.heappop(a) != 0:
+c = n%2
+if c and heapq.heappop(a) != 0:
     print(0)
     exit()
-for _ in range(n//2):
-    if heapq.heappop(a) != heapq.heappop(a):
+for i in range(1, n, 2):
+    if heapq.heappop(a) != i+c or heapq.heappop(a) != i+c:
         print(0)
         exit()
-print(pow(2, n//2, mod))
+print(pow(2, n//2, 10**9+7))
